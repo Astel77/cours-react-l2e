16966df371055f20921e9df82b52853659e84930
mamecheikhndiaye992-max <mamecheikhndiaye992@gmail.com>
2026-05-14 23:10:18 +0000

    fix: export de setUser pour login et register

--- src/Pages/Register.tsx
+++ src/Pages/Register.tsx
@@ -1,25 +1,61 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { setUser } from "../utils/auth";
+import { registerUser } from "../utils/auth";
 
-export default function Register(){
-  const [form,setForm]=useState({nom:"",prenom:"",email:"",password:""});
+export default function Register() {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   const nav = useNavigate();
 
-  const handle = ()=>{
-    setUser(form);
-    nav("/");
+  const handleRegister = () => {
+
+    if (!email || !password) {
+      alert("Remplissez tous les champs");
+      return;
+    }
+
+    registerUser({
+      email,
+      password,
+    });
+
+    alert("Compte créé ✅");
+
+    nav("/login");
   };
 
   return (
-    <div className="flex justify-center items-center h-screen bg-black text-white">
-      <div className="bg-gray-800 p-6 rounded">
-        <input placeholder="Nom" className="block mb-2 p-2 text-black" onChange={e=>setForm({...form,nom:e.target.value})}/>
-        <input placeholder="Prenom" className="block mb-2 p-2 text-black" onChange={e=>setForm({...form,prenom:e.target.value})}/>
-        <input placeholder="Email" className="block mb-2 p-2 text-black" onChange={e=>setForm({...form,email:e.target.value})}/>
-        <input type="password" placeholder="Password" className="block mb-2 p-2 text-black" onChange={e=>setForm({...form,password:e.target.value})}/>
+    <div className="bg-black min-h-screen flex items-center justify-center">
 
-        <button onClick={handle} className="bg-green-600 p-2 w-full">Créer compte</button>
+      <div className="bg-white p-8 rounded-3xl w-[380px] border-4 border-yellow-400">
+
+        <h1 className="text-4xl font-bold mb-6 text-black">
+          Inscription
+        </h1>
+
+        <input
+          type="email"
+          placeholder="Email"
+          className="w-full border-2 border-yellow-400 p-3 rounded-xl mb-4 text-black"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <input
+          type="password"
+          placeholder="Mot de passe"
+          className="w-full border-2 border-yellow-400 p-3 rounded-xl mb-6 text-black"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button
+          onClick={handleRegister}
+          className="bg-yellow-400 w-full py-3 rounded-xl font-bold text-black"
+        >
+          Créer un compte
+        </button>
+
       </div>
     </div>
   );
